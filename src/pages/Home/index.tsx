dbee671ae1e1fb6cf1c3b01a1a17c3ef2f341945
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-06-27 21:39:42 -0300

    feat(index): implement tags

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,40 @@
+import { ReactComponent as Search } from "assets/icons/search.svg";
+import * as S from "./style";
+
 const Home = () => {
     return (
-        <div>
-            Componente Home
-        </div>
+        <S.Home>
+            <S.HomeContent>
+                <header>
+                    <S.HomeHeaderDetails>
+                        <div>
+                            <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
+                            <S.HomeHeaderDetailsDate>
+                                Aqui ficará a data
+                            </S.HomeHeaderDetailsDate>
+                        </div>
+                        <S.HomeHeaderDetailsSearch>
+                            <Search />
+                            <input type="text" placeholder="Procure pelo sabor" />
+                        </S.HomeHeaderDetailsSearch>
+                    </S.HomeHeaderDetails>
+                </header>
+                <div>
+                    <S.HomeProductTitle>
+                        <b>Pizzas</b>
+                    </S.HomeProductTitle>
+                    <S.HomeProductList>
+                        <p>Lista de produtos aqui</p>
+                    </S.HomeProductList>
+                </div>
+            </S.HomeContent>
+            <aside>
+                <p>
+                    Detalhes dos pedidos aqui
+                </p>
+            </aside>
+        </S.Home>
     );
-}
+};
 
 export default Home;
